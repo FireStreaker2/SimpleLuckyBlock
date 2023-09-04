721f8ff706123dbc8186a918d5ec0302e3837a37
FireStreaker2 <michaelccc2019@outlook.com>
2023-09-04 16:22:14 -0700

    textures

--- src/main/java/firestreaker2/simple/lucky/block/SimpleLuckyBlock.java
+++ src/main/java/firestreaker2/simple/lucky/block/SimpleLuckyBlock.java
@@ -1,23 +1,24 @@
 package firestreaker2.simple.lucky.block;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.util.Identifier;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-
-// import java.rmi.registry.Registry;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.minecraft.sound.BlockSoundGroup;
 
 public class SimpleLuckyBlock implements ModInitializer {
   // This logger is used to write text to the console and the log file.
   // It is considered best practice to use your mod id as the logger's name.
   // That way, it's clear which mod wrote info, warnings, and errors.
-  public static final Logger SIMPLELUCKYBLOCK = LoggerFactory.getLogger("modid");
-  public static final Block SIMPLE_LUCKY_BLOCK = new Block(FabricBlockSettings.create().strength(4.0f));
+
+  // public static final Item SIMPLE_LUCKY_BLOCK_ITEM = new Item(new Item.Settings());
+  public static final Block SIMPLE_LUCKY_BLOCK = new Block(FabricBlockSettings.create().strength(4.0f, 1200.0f).sounds(BlockSoundGroup.STONE));
 
 
 
@@ -27,8 +28,13 @@ public class SimpleLuckyBlock implements ModInitializer {
     // However, some things (like resources) may still be uninitialized.
     // Proceed with mild caution.
 
+    // Registry.register(Registries.ITEM, new Identifier("simpleluckyblock", "simple_lucky_block"), SIMPLE_LUCKY_BLOCK_ITEM);
     Registry.register(Registries.BLOCK, new Identifier("simpleluckyblock", "simple_lucky_block"), SIMPLE_LUCKY_BLOCK);
+    Registry.register(Registries.ITEM, new Identifier("simpleluckyblock", "simple_lucky_block"), new BlockItem(SIMPLE_LUCKY_BLOCK, new Item.Settings()));
 
-    SIMPLELUCKYBLOCK.info("block has been registered");
+    ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(content -> {
+      content.add(SIMPLE_LUCKY_BLOCK);
+    });
+
   }
 }
