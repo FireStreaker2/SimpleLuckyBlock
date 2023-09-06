e3a927c60535ad965d99c5de1006e1ba7889ae22
FireStreaker2 <michaelccc2019@outlook.com>
2023-09-06 15:38:22 -0700

    drop logic

--- src/main/java/firestreaker2/simple/lucky/block/LuckyBlock.java
+++ src/main/java/firestreaker2/simple/lucky/block/LuckyBlock.java
@@ -2,10 +2,15 @@ package firestreaker2.simple.lucky.block;
 
 import java.util.Random;
 
+import com.google.common.collect.Iterables;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.text.Text;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -19,7 +24,17 @@ public class LuckyBlock extends Block {
     if (!world.isClient) {
       Random random = new Random();
       int amount = random.nextInt(64);
-      player.sendMessage(Text.of("your lucky number is: " + amount), false);
+
+      Iterable<Identifier> ids = Registries.ITEM.getIds();
+      Identifier[] idArray = Iterables.toArray(ids, Identifier.class);
+      int index = random.nextInt(idArray.length);
+      Item item = Registries.ITEM.get(idArray[index]);
+
+      ItemStack items = new ItemStack(item, amount);
+
+      if (!player.getInventory().insertStack(items)) {
+        player.dropItem(items, false);
+      }
     }
 
     super.onBreak(world, pos, state, player);
